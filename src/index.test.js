4eac4db141e324d7cb1ae91cc07a80f24f11e751
Leo Javier <leo@impactrepublic.com>
2017-12-19 15:35:21 -0500

    refactor(fixing travis):

--- src/index.test.js
+++ src/index.test.js
@@ -3,7 +3,7 @@ const helper = require('./index')
 
 describe('first test', () => {
   it('should return true', () => {
-    expect(true).to.be.true
+    expect(false).to.be.true
   });
 });
 
@@ -12,7 +12,7 @@ describe('Reverse array module', () => {
   it('should return sentence with words in reverse', () => {
     var str = 'leo javier'
     var str2 = 'Maryann javier'
-    
+
     expect(helper.reverseWords(str)).to.equal('oel reivaj')
     expect(helper.reverseWords(str2)).to.equal('nnayraM reivaj')
   });
